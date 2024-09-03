f8097ef26ccf8abfde6db79e62357c9742f046e1
Pedro H Mazzilli <pedrohmaz@gmail.com>
2024-09-03 13:22:07 -0300

    classes em kotlin

---
+++ app/src/main/java/com/example/ebac1/ClassesEmKotlin.kt
@@ -0,0 +1,40 @@
+package com.example.ebac1
+
+fun main() {
+
+    val car = Car("Fusca")
+
+    println(car.getSpeed()) //0
+    car.speedUp()
+    println(car.getSpeed()) //10
+    car.speedDown()
+    println(car.getSpeed()) //0
+    car.speedDown()
+    println(car.getSpeed()) //0
+    car.speedUp()
+    println(car.getSpeed()) //10
+    car.speedUp()
+    println(car.getSpeed()) //20
+
+}
+
+
+class Car(
+    private val model: String,
+    private var speed: Long = 0,
+    private val acc: Long = 10
+) {
+
+    fun speedUp() {
+        this.speed += acc
+    }
+
+    fun speedDown() {
+        this.speed -= acc
+        if (this.speed < 0)
+            this.speed = 0
+    }
+
+    fun getSpeed() = this.speed
+
+}
